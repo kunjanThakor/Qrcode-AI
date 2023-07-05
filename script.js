ecc45ec0518ade7f03b4117c9f692be490d54784
Kunjan Thakor <80092583+kunjanThakor@users.noreply.github.com>
2023-07-05 20:14:40 +0530

    updated

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const generateQr = () => {
     debugger;
     let url = document.getElementById('url');
     let qr = document.querySelector('#qr');
+    let btnClr = document.querySelector('#clr');
     qr.style = "visibility: visible";
     qr.src = "https://api.qrserver.com/v1/create-qr-code/?data=" + url.value;
     url.value='';
+    btnClr.style = "visibility: visible";
+}
+
+const reset=()=>{
+    let qr = document.querySelector('#qr');
+    let btnClr = document.querySelector('#clr');
+    qr.style = "visibility: hidden";
+    btnClr.style = "visibility: hidden";
 }
